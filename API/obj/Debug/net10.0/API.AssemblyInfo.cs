78154fee559b954aa43d76bb7403eff51b2978f6
ashikHstu <ashik.cse.hstu@gmail.com>
2026-01-24 21:53:25 +0600

    API call created

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0814c09561816558aa56ec00a62506f70815e2ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609d540507f41746bd65055c05ef7ea294699704")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
